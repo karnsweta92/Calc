76669dfbfbe8af3319d18c9d63fc210e3180187a
Sweta Karn <karn.sweta92@gmail.com>
2020-09-20 12:49:57 +0530

    calc changes

---
+++ calc.js
@@ -0,0 +1,30 @@
+var inp1;
+var ans;
+var operation;
+
+
+function getInput(){
+
+val=document.querySelector("btn").addEventListener("click",displayData );
+
+function displayData(val){
+    document.getElementById("data").value=val;
+
+}
+
+}
+function clr() 
+         { 
+             document.getElementById("answer").value = "" 
+         } 
+         function add(){
+             inp1=document.querySelector("btn").addEventListener("click",displayData );
+             if (ans!==0)
+             {
+             ans=ans+imp1;
+             }
+             else{
+                document.querySelector("btn").addEventListener("click",displayData );
+             }
+         }
+
