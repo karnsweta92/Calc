185a80cdd6858cc2b78f6a3463fc6b0146458b97
Sweta Karn <karn.sweta92@gmail.com>
2020-09-30 22:45:27 +0530

    Change-Addition of more buttons

--- calc.js
+++ calc.js
@@ -2,29 +2,32 @@ var inp1;
 var ans;
 var operation;
 
+var buttons=document.querySelector(".buttons");
 
-function getInput(){
+buttons.addEventListener("click",function (e){
+    var key=e.target.textContent;
+    
+    console.log("Button pressed: "+ key );
+    var ans= document.getElementById("data")
+    console.log(ans);
+    ans.textContent=key;
+    
 
-val=document.querySelector("btn").addEventListener("click",displayData );
-
-function displayData(val){
-    document.getElementById("data").value=val;
-
-}
-
-}
+} );
 function clr() 
          { 
              document.getElementById("answer").value = "" 
          } 
-         function add(){
-             inp1=document.querySelector("btn").addEventListener("click",displayData );
+
+function add()
+{
+             inp1=document.querySelector(".btn").addEventListener("click",displayData );
              if (ans!==0)
              {
              ans=ans+imp1;
              }
              else{
-                document.querySelector("btn").addEventListener("click",displayData );
+                document.querySelector(".btn").addEventListener("click",displayData );
              }
          }
 
